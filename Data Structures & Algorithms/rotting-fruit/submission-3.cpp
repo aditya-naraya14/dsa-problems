c4f67c3c2e6f77770a2678bad12fd13fb3868439
Aditya Narayan <42352133+aditya-naraya14@users.noreply.github.com>
2026-09-09 02:02:32 +0530

    Add: rotting-fruit - submission-3

---
+++ Data Structures & Algorithms/rotting-fruit/submission-3.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    const int ROTTEN = 2;
+    const int FRESH = 1;
+
+    const vector<pair<int, int>> DIRS = {{1,0}, {0,1}, {0,-1}, {-1,0}};
+    bool isInvalid(int x, int n) {
+        return x < 0 || x == n;
+    }
+
+    int orangesRotting(vector<vector<int>>& grid) {
+        const int n = grid.size(), m = grid[0].size();
+        queue<pair<int,int>> q;
+        int freshFruits = 0;
+
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<m; j++) {
+                if (grid[i][j] == FRESH) freshFruits++;
+                else if (grid[i][j] == ROTTEN) {
+                    q.push({i, j});
+                }
+            }
+        }
+
+        if (freshFruits == 0) return 0;
+
+        int time = 0;
+        while(!q.empty()) {
+            int size = q.size();
+            if (freshFruits == 0) return time;
+
+            for(int i=0; i<size; i++) {
+                int ux = q.front().first, uy = q.front().second;
+                q.pop();
+                for (auto [x, y]: DIRS) {
+                    int vx = ux + x, vy = uy + y;
+                    if (isInvalid(vx, n) || isInvalid(vy, m)) continue;
+
+                    if (grid[vx][vy] == FRESH) {
+                        grid[vx][vy] = ROTTEN;
+                        q.push({ vx, vy });
+                        freshFruits--;
+                    }
+                }
+            }
+            time++;
+        }
+
+        return -1; 
+    }
+};
