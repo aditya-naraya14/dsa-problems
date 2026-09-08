0c55b361c82f4864395fd06c9d86a76f08879921
Aditya Narayan <42352133+aditya-naraya14@users.noreply.github.com>
2026-09-09 01:36:31 +0530

    Add: islands-and-treasure - submission-1

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-1.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    const int LAND = 2147483647;
+    const int TREASURE = 0;
+    const vector<vector<int>> DIRS = {{1,0}, {0,1}, {-1,0}, {0,-1}};
+
+    bool isInvalid(int x, int n) {
+        return x < 0 || x >= n;
+    }
+
+    void islandsAndTreasure(vector<vector<int>>& grid) {
+        const int n = grid.size(), m = grid[0].size();
+        queue<vector<int>> q;
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<m; j++) {
+                if (grid[i][j] == TREASURE) {
+                    q.push({i, j});
+                }
+            }
+        }
+
+        int dist = 1;
+        while(!q.empty()) {
+            int size = q.size();
+
+            for (int i=0; i<size; i++) {
+                int ux = q.front()[0], uy = q.front()[1];
+                q.pop();
+
+                for (vector<int> dir: DIRS) {
+                    int vx = ux + dir[0], vy = uy + dir[1];
+                    if (isInvalid(vx, n) || isInvalid(vy, m)) continue;
+
+                    if (grid[vx][vy] == LAND) {
+                        grid[vx][vy] = dist;
+                        q.push({vx, vy});
+                    }
+                }
+            }
+            dist++;
+        }
+    }
+};
